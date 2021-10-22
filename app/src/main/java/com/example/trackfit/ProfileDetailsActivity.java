7f9d9b75327e22b508e895203d0d2a2c06d36704
Eric Sterwald <esterwald@wisc.edu>
2021-10-22 14:14:04 -0500

    Added an activity for all screens in application

---
+++ app/src/main/java/com/example/trackfit/ProfileDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.trackfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_details);
+    }
+}

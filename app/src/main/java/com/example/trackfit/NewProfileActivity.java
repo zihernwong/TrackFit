9852e13c7b163a5a1e4599acdb78f4c78ffc9e50
Eric Sterwald <esterwald@wisc.edu>
2021-10-22 14:24:11 -0500

    Fixed errors in file names and manifest

--- app/src/main/java/com/example/trackfit/NewProfileActivity.java
+++ app/src/main/java/com/example/trackfit/NewProfileActivity.java
@@ -9,6 +9,6 @@ public class NewProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_profile2);
+        setContentView(R.layout.activity_new_profile);
     }
 }

b3cddaf6aba5041e978210c185f9b8063d165acd
its-raining <xfei5@wisc.edu>
2021-10-23 10:56:33 -0500

    Milestone 1 (Have menu bar display across all activities)

--- app/src/main/java/com/example/trackfit/WorkoutActivity.java
+++ app/src/main/java/com/example/trackfit/WorkoutActivity.java
@@ -1,8 +1,12 @@
 package com.example.trackfit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 public class WorkoutActivity extends AppCompatActivity {
 
@@ -11,4 +15,27 @@ public class WorkoutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
     }
+
+    // Bottom navigation bar selecting one of the three activities (Xiao)
+    private NavigationBarView.OnItemSelectedListener bottomnavFunction = new NavigationBarView.OnItemSelectedListener(){
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.previous_workout:
+                    fragment = new PreviousWorkoutFragment();
+                    break;
+                case R.id.workout:
+                    fragment = new WorkoutFragment();
+                    break;
+                case R.id.profile:
+                    fragment = new ProfileFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+            return true;
+        }
+    };
+
+
 }
---
+++ app/src/main/java/com/example/trackfit/WorkoutFragment.java
@@ -0,0 +1,23 @@
+package com.example.trackfit;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WorkoutFragment extends Fragment {
+
+    public WorkoutFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout, container, false);
+    }
+}

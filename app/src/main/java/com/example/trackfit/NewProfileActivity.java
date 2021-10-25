b075c6008d122a16a04852680cdace3e95aa7962
Eric Sterwald <esterwald@wisc.edu>
2021-10-25 14:43:41 -0500

    Added code for location request and changed main screen to NewProfileActivity

--- app/src/main/java/com/example/trackfit/NewProfileActivity.java
+++ app/src/main/java/com/example/trackfit/NewProfileActivity.java
@@ -1,14 +1,76 @@
 package com.example.trackfit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class NewProfileActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_profile);
+
+        //TODO: Will check shared preferences for profile details. If none are found we display this page
+        // where a new user will enter their details. If details are found then we display the WorkoutActivity
+        // but we still need to get location.
+        boolean profileFound = true; // Hardcoded for testing until functionality is implemented
+
+        locationStartup();
+
+         if (profileFound) {
+             displayWorkout();
+         }
+
+    }
+
+    public void locationStartup() {
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                // TODO
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {}
+            @Override
+            public void onProviderEnabled(String s) {}
+            @Override
+            public void onProviderDisabled(String s) {}
+        };
+
+        if (Build.VERSION.SDK_INT < 23) {
+            //TODO
+        } else {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location != null) {
+                    //TODO
+                }
+            }
+        }
+    }
+
+    public void displayWorkout() {
+        Intent intent = new Intent(this, WorkoutActivity.class);
+        startActivity(intent);
     }
 }

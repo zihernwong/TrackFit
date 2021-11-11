b1c1087f5b2d8e4975600a01e8d07207c86375e5
jiaxinli <jli2274@wisc.edu>
2021-11-11 10:26:19 -0600

    Jiaxin's work until Nov 11

--- app/src/main/java/com/example/trackfit/CurrentWorkoutFragment.java
+++ app/src/main/java/com/example/trackfit/CurrentWorkoutFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CurrentWorkoutFragment extends Fragment {
 
--- app/src/main/java/com/example/trackfit/WorkoutFragment.java
+++ app/src/main/java/com/example/trackfit/WorkoutFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Intent;
 
 public class WorkoutFragment extends Fragment {
 
@@ -20,4 +21,7 @@ public class WorkoutFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout, container, false);
     }
+
+
+
 }

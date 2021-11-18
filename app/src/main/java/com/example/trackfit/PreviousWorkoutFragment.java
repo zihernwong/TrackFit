3f899de9cd7d208cda48b89fdfebc31e6dd7cbf1
Xiao <xfei5@wisc.edu>
2021-11-18 14:17:33 -0600

    11.18 Previous workout and details static button & back delete button
    1. Previous workout and workout details Activities have static buttons and text
    2. Back button on previous workout page performs the correct action
    3. Delete button on previous workout page opens the alert box

--- app/src/main/java/com/example/trackfit/PreviousWorkoutFragment.java
+++ app/src/main/java/com/example/trackfit/PreviousWorkoutFragment.java
@@ -1,14 +1,24 @@
 package com.example.trackfit;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class PreviousWorkoutFragment extends Fragment {
+    private ArrayList<PreviousWorkout> previousWorkoutsList;
+    private RecyclerView recyclerView;
+    private PreviousWorkoutRecyclerAdapter.RecyclerViewClickListener listener;
 
     public PreviousWorkoutFragment() {
         // Required empty public constructor
@@ -18,6 +28,45 @@ public class PreviousWorkoutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_previous_workout, container, false);
+        previousWorkoutsList = new ArrayList<>();
+        View view = inflater.inflate(R.layout.fragment_previous_workout, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView);
+
+        setPreviousWorkoutInfo();
+        setAdapter();
+
+        return view;
+    }
+
+    private void setAdapter() {
+        setOnClickListener();
+        PreviousWorkoutRecyclerAdapter adapter = new PreviousWorkoutRecyclerAdapter(previousWorkoutsList, listener);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setOnClickListener() {
+        listener = new PreviousWorkoutRecyclerAdapter.RecyclerViewClickListener() {
+            @Override
+            public void onClick(View v, int position) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container, new PreviousWorkoutDetailsFragment()).commit();
+            }
+        };
+    }
+
+    private void setPreviousWorkoutInfo() {
+        previousWorkoutsList.add(new PreviousWorkout("2020/11/18","0:01:00"));
+        previousWorkoutsList.add(new PreviousWorkout("2020/11/17","0:02:00"));
+        previousWorkoutsList.add(new PreviousWorkout("2020/11/16","0:03:10"));
+        previousWorkoutsList.add(new PreviousWorkout("2020/11/16","0:03:10"));
+        previousWorkoutsList.add(new PreviousWorkout("2020/11/16","0:03:10"));
+        previousWorkoutsList.add(new PreviousWorkout("2020/11/16","0:03:10"));
+        previousWorkoutsList.add(new PreviousWorkout("2020/11/16","0:03:10"));
+        previousWorkoutsList.add(new PreviousWorkout("2020/11/16","0:03:10"));
+        previousWorkoutsList.add(new PreviousWorkout("2020/11/16","0:03:10"));
+        previousWorkoutsList.add(new PreviousWorkout("2020/11/16","0:03:10"));
+        previousWorkoutsList.add(new PreviousWorkout("2020/11/16","0:03:10"));
     }
 }

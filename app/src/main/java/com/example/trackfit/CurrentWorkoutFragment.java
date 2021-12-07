39dcc92e81d7f9b7b46148dbebc0094da41f10e3
Eric Sterwald <esterwald@wisc.edu>
2021-12-07 11:45:45 -0600

    Attempted fix for step counting

--- app/src/main/java/com/example/trackfit/CurrentWorkoutFragment.java
+++ app/src/main/java/com/example/trackfit/CurrentWorkoutFragment.java
@@ -1,20 +1,26 @@
 package com.example.trackfit;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -49,6 +55,10 @@ public class CurrentWorkoutFragment extends Fragment implements View.OnClickList
         stopWorkoutButton.setOnClickListener(this);
 
         // Getting the step sensor
+
+        if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 1);
+        }
         Sensor stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
         sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI);
 
@@ -71,6 +81,7 @@ public class CurrentWorkoutFragment extends Fragment implements View.OnClickList
     // When the sensor detects a step, update the steps taken value
     @Override
     public void onSensorChanged(SensorEvent event) {
+        Log.d("Sensor", "step detected");
         stepsTaken = event.values[0];
         currentDistanceTextView.setText((int) stepsTaken);
     }
